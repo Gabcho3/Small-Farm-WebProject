6946be9225010f3ebbc7ba22a7268327acd2570c
Gabriel Dimitrov <gabriel_work@abv.bg>
2024-02-25 20:16:45 +0200

    Unnecessary code removed

--- SmallFarm/SmallFarm.Web/Program.cs
+++ SmallFarm/SmallFarm.Web/Program.cs
@@ -4,13 +4,6 @@ using Microsoft.EntityFrameworkCore;
 using SmallFarm.Data;
 
 var builder = WebApplication.CreateBuilder(args);
-var connectionString = builder.Configuration.GetConnectionString("SmallFarmDbContextConnection") ?? throw new InvalidOperationException("Connection string 'SmallFarmDbContextConnection' not found.");
-
-builder.Services.AddDbContext<SmallFarmDbContext>(options =>
-    options.UseSqlServer(connectionString));
-
-builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
-    .AddEntityFrameworkStores<SmallFarmDbContext>();
 
 builder.Services.AddApplicationDbContext(builder.Configuration);
 builder.Services.AddApplicationIdentity();
