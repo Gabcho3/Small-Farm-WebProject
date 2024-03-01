6e8cf6ab04e18da91fb8a9a4b151632af18e9c37
Gabriel Dimitrov <gabriel_work@abv.bg>
2024-03-01 22:34:43 +0200

    Visaulisation for Manufacturer Add page added

--- SmallFarm/SmallFarm/Controllers/ManufacturerController.cs
+++ SmallFarm/SmallFarm/Controllers/ManufacturerController.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using SmallFarm.Core.Contracts;
+using SmallFarm.Core.Models;
 using SmallFarm.Core.Services;
+using SmallFarm.Models;
 
 namespace SmallFarm.Controllers
 {
@@ -19,6 +21,27 @@ namespace SmallFarm.Controllers
             var allManufacturers = await service.GetAllManufacturersAsync();
 
             return View(allManufacturers);
+        }
+
+        [HttpGet]
+        public IActionResult Add()
+        {
+            var model = new ManufacturerViewModel();
+
+            return View(model);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Add(ManufacturerViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            await service.AddManufacturerAsync(model);
+
+            return RedirectToAction("Index");
         }
     }
 }
