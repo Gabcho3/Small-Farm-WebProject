0002df6a3a18d0dded333c551f203e86946cfcf3
Gabriel Dimitrov <gabriel_work@abv.bg>
2024-03-01 23:38:34 +0200

    Businness Logic for Manufacturer Edit page added

--- SmallFarm/SmallFarm.Core/Contracts/IManufacturerService.cs
+++ SmallFarm/SmallFarm.Core/Contracts/IManufacturerService.cs
@@ -4,8 +4,12 @@ namespace SmallFarm.Core.Contracts
 {
     public interface IManufacturerService
     {
+        Task<ManufacturerViewModel> GetManufacturerByIdAsync(Guid id);
+
         Task<IEnumerable<ManufacturerViewModel>> GetAllManufacturersAsync();
 
         Task AddManufacturerAsync(ManufacturerViewModel model);
+
+        Task EditManufacturerAsync(Guid id, ManufacturerViewModel model);
     }
 }
--- SmallFarm/SmallFarm.Core/Services/ManufacturerService.cs
+++ SmallFarm/SmallFarm.Core/Services/ManufacturerService.cs
@@ -22,9 +22,24 @@ namespace SmallFarm.Core.Services
             }));
         }
 
+        public async Task<ManufacturerViewModel> GetManufacturerByIdAsync(Guid id)
+        {
+            var manufacturer = await context.Manufacturers
+                .Include(m => m.Location)
+                .Where(m => m.Id == id)
+                .FirstAsync();
+
+            var model = autoMapper.Map<ManufacturerViewModel>(manufacturer);
+            model.Address = manufacturer!.Location.Address;
+            model.City = manufacturer.Location.City;
+
+            return model;
+        }
+
         public async Task<IEnumerable<ManufacturerViewModel>> GetAllManufacturersAsync()
         {
             var allManufacturers = await context.Manufacturers
+                .AsNoTracking()
                 .Include(m => m.Location)
                 .OrderByDescending(m => m)
                 .Select(m => autoMapper.Map<ManufacturerViewModel>(m))
@@ -36,6 +51,7 @@ namespace SmallFarm.Core.Services
         public async Task AddManufacturerAsync(ManufacturerViewModel model)
         {
             Manufacturer manufacturer = autoMapper.Map<Manufacturer>(model);
+
             manufacturer.Location = new Location()
             {
                 Address = model.Address,
@@ -43,6 +59,23 @@ namespace SmallFarm.Core.Services
             };
 
             await context.AddAsync(manufacturer);
+            await context.SaveChangesAsync();
+        }
+
+        public async Task EditManufacturerAsync(Guid id, ManufacturerViewModel model)
+        {
+            var manufacturerToEdit = await context.Manufacturers
+                .Include(m => m.Location)
+                .Where(m => m.Id == id)
+                .FirstAsync();
+
+            manufacturerToEdit!.Name = model.Name;
+            manufacturerToEdit.Description = model.Description;
+            manufacturerToEdit.PhoneNumber = model.PhoneNumber;
+            manufacturerToEdit.Email = model.Email;
+            manufacturerToEdit.Location.Address = model.Address;
+            manufacturerToEdit.Location.City = model.City;
+
             await context.SaveChangesAsync();
         }
     }
