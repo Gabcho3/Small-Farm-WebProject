8de23252eb84ea07559fae71d24384bed8e71a46
Gabriel Dimitrov <gabriel_work@abv.bg>
2024-04-10 23:43:33 +0300

    Mocks and usings added

---
+++ SmallFarm/SmallFarm.Tests/Mocks/DatabaseMock.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SmallFarm.Data;
+
+namespace SmallFarm.Tests.Mocks
+{
+    public static class DatabaseMock
+    {
+        public static SmallFarmDbContext Instance
+        {
+            get
+            {
+                var dbContextOptions = new DbContextOptionsBuilder<SmallFarmDbContext>()
+                    .UseInMemoryDatabase("SmallFarm")
+                    .Options;
+
+                return new SmallFarmDbContext(dbContextOptions);
+            }
+        }
+    }
+}
---
+++ SmallFarm/SmallFarm.Tests/Mocks/UserManagerMock.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Moq;
+using SmallFarm.Data.Entities;
+
+namespace SmallFarm.Tests.Mocks
+{
+    public static class UserManagerMock
+    {
+        public static Mock<UserManager<ApplicationUser>> Instance
+        {
+            get
+            {
+                var userStoreMock = new Mock<IUserStore<ApplicationUser>>();
+
+                return new Mock<UserManager<ApplicationUser>>(
+                    userStoreMock.Object,
+                    null, // You can add other parameters here if needed
+                    null,
+                    null,
+                    null,
+                    null,
+                    null,
+                    null,
+                    null);
+            }
+        }
+    }
+}
